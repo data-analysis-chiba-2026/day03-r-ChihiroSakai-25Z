70ee206b1a13b6edf2e8039014b2d7f26ec57006
ChihiroSakai-25Z <25zb1038@student.gs.chiba-u.jp>
2026-07-02 00:06:02 +0900

    Finish homework

--- day03_homework.R
+++ day03_homework.R
@@ -1,6 +1,6 @@
 # DAY 3 HOMEWORK
 #
-# Name: Joel Nitta
+# Name: Chihiro Sakai
 
 # 0. This demonstrates how to save your answers.
 # I've written the answer for you.
@@ -28,11 +28,13 @@ answer_0 <- 365 * 5
 weight_lb <- 160
 height_ft <- 6
 
+answer_1 <- (weight_lb * 0.4536) / (height_ft * 0.3048) ^2
+
 # 2. The following code has an error. Fix it by editing the code.
 # (You don't need to save a separate answer for this one, just fix the code.)
 
 volume_oz <- 130
-volume_ml <- Volume_oz * 29.574
+volume_ml <- Volume_oz * 28.3495
 
 # 3. What is the value of z after running the following code?
 #
@@ -43,10 +45,14 @@ y <- 3
 z <- x + y
 x <- 5
 
+answer_3 <- z
+
 # 4. The sqrt() function calculates the square root of a number.
 #
 # Use the sqrt() function to calculate the square root of 200, and
 # save your answer in an object called answer_4.
+
+answer_4 <- sqrt(200)
 
 # 5. The seq() function generates sequences of numbers.
 # Its arguments include "from" (the starting number), "to"
@@ -56,10 +62,18 @@ x <- 5
 # Use seq() make a sequence of even numbers starting at 2 and ending at 100, and
 # save your answer in an object called answer_5
 
+answer_5 <- seq(from = 2, to = 100, by = 1)
+  
 # 6. Make a vector of logical values (TRUE and FALSE), and save it to an
 #  object called lgl_vec
+
+lgl_vec <- c(TRUE, FALSE)
+
 #  Next, make vector of character values, and save it to an object
 #  called called chr_vec
+
+chr_vec <- c("abc")
+
 #  Next, combine lgl_vec and chr_vec.
 #
 # What is the type of vector that you obtain? Check with the typeof() function.
@@ -68,6 +82,9 @@ x <- 5
 #
 # HINT: use c() to make vectors
 # HINT: characters are always written with quotation marks
+
+mixed <- c(lgl_vec, chr_vec)
+answer_6 <- typeof(mixed)
 
 # 7. R includes some pre-defined vectors, like `letters`:
 letters
@@ -78,3 +95,5 @@ letters
 # Save your answer in an object called answer_7
 #
 # HINT: Use `seq()` inside the square brackets
+
+answer_7 <- letters[seq(from = 1, to = 26, by = 1)]
